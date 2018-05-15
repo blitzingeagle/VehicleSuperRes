938372dce77b6a80b2f48fd00987ee750b79bf69
blitzingeagle <mcmorrischen@gmail.com>
2018-05-15 16:17:47 +0800

    Implement constructor and __len__ in VideoDataset

--- VideoDataset.py
+++ VideoDataset.py
@@ -1,11 +1,15 @@
+from glob import glob
+
 from torch.utils.data import Dataset
 
 class VideoDataset(Dataset):
-    def __init__(self):
-        None
+    def __init__(self, video_dir, transform):
+        self.video_list = glob(video_dir + "/*.avi")
+        self.video_dir = video_dir
+        self.transform = transform
 
     def __len__(self):
-        None
+        return len(self.video_list)
 
     def __getitem__(self, idx):
         None
