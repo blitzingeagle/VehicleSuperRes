e49c7ae16ecc6e7b209040691de5bde0bf3d88e2
blitzingeagle <mcmorrischen@gmail.com>
2018-05-15 16:22:37 +0800

    Complete VideoDataset Implementation

--- VideoDataset.py
+++ VideoDataset.py
@@ -1,5 +1,9 @@
+import cv2
+
+import os.path as path
 from glob import glob
 
+import torch
 from torch.utils.data import Dataset
 
 class VideoDataset(Dataset):
@@ -12,7 +16,38 @@ class VideoDataset(Dataset):
         return len(self.video_list)
 
     def __getitem__(self, idx):
-        None
+        videoFile = path.join(self.rootDir, self.clipsList[idx][0])
+        clip, failedClip = self.readVideo(videoFile)
+        if self.transform:
+            clip = self.transform(clip)
+        sample = {'clip': clip, 'label': self.clipsList[idx][1], 'failedClip': failedClip}
+
+        return sample
 
     def readVideo(self, videoFile):
-        None
+        # Open the video file
+        cap = cv2.VideoCapture(videoFile)
+
+        channels = 3
+        time_depth = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+        frames = torch.FloatTensor(channels, time_depth, width, height)
+
+        failedClip = False
+        for f in range(time_depth):
+            ret, frame = cap.read()
+            if ret:
+                frame = torch.from_numpy(frame)
+                # HWC2CHW
+                frame = frame.permute(2, 0, 1)
+                frames[:, f, :, :] = frame
+
+            else:
+                print("Skipped!")
+                failedClip = True
+                break
+
+        frames /= 255
+        return frames, failedClip
