a6db73c647b584eea4aea65d95c63e97ea7b9e61
blitzingeagle <mcmorrischen@gmail.com>
2018-05-21 09:08:09 +0800

    Video upscale condition

--- convert.py
+++ convert.py
@@ -180,7 +180,7 @@ def video():
                     input = input[:,:,::-1]
                     input = np.swapaxes(np.swapaxes(np.array(input, dtype=float), 0, 2), 1, 2) / 255.0
                     input = torch.from_numpy(input.reshape((1,) + input.shape)).float().cuda()
-                    output = upscale_model(input)
+                    output = input if args.no_upscale else upscale_model(input)
                     if args.verbose:
                         if args.type == "image":
                             print("{0}/{1}:\t{2} {3} --> {4} {5}".format(idx, time_depth, video, tuple(input.shape), filepath, tuple(output.shape)))
